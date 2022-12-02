c8f7467923ab9ff0a34eb9443e258968cadc35a9
Wittmann Mátyás <wittmann.matyas@gmail.com>
2022-12-02 23:08:43 +0100

    day-2 task-1 solutions

---
+++ Day 2/task1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+int calculatePoints(char enemy, char move)
+{
+    unsigned ans = 0;
+    ans = int(move) - 87;
+    if (int(enemy) == (int(move) - 23))
+        ans += 3;
+    if ((enemy == 'C' && move == 'X') || (enemy == 'A' && move == 'Y') || (enemy == 'B' && move == 'Z'))
+        ans += 6;
+    return ans;
+}
+
+int main()
+{
+    std::ifstream file("dataset.txt");
+    std::string str;
+    int ans = 0;
+    while (std::getline(file, str))
+    {
+        ans += calculatePoints(str[0], str[2]);
+    }
+    cout << ans;
+    return 0;
+}
