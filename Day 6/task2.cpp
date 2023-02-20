90fd0ab9ca63459b998d8bb2458adaf38f03da50
Wittmann Mátyás <wittmann.matyas@gmail.com>
2023-02-20 14:59:16 +0100

    Update task2.cpp

--- Day 6/task2.cpp
+++ Day 6/task2.cpp
@@ -1,32 +1,38 @@
+#include <bits/stdc++.h>
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 
 using namespace std;
 
-bool isOverlap(int a1, int a2, int b1, int b2)
-{
-    if ((a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2) || (a2 >= b1 && a2 <= b2) || (a1 <= b2 && b2 <= a2))
-        return true;
-    return false;
+bool check_if_cont(int data[14]) {
+  int temp[14] = {0};
+  for (int i = 0; i < 14; i++)
+    temp[i] = data[i];
+  int arraySize = sizeof(temp) / sizeof(temp[0]);
+  sort(temp, temp + arraySize, greater<int>());
+  for (int i = 0; i < 14; i++)
+    if (temp[i] == temp[i + 1])
+      return true;
+  return false;
 }
 
-int main()
-{
-    std::ifstream file("dataset.txt");
-    std::string str;
-    int ans, i = 0;
-    int dataset[4] = {0};
-    ans = 0;
-    while (file >> str)
-    {
-        dataset[i] = stoi(str);
-        i++;
-        if (i > 3)
-        {
-            ans += isOverlap(dataset[0], dataset[1], dataset[2], dataset[3]);
-            i = 0;
-        }
-    }
-    cout << ans;
+int task(string data) {
+  int temp[14] = {0};
+  for (int i = 0; i < data.length(); i++) {
+    for (int j = 0; j < 14; j++)
+      temp[j] = data[i + j];
+    if (check_if_cont(temp) == false)
+      return i + 14;
+  }
+  return 0;
+}
+
+int main() {
+  std::ifstream file("dataset.txt");
+  std::string str;
+  int ans = 0;
+  while (std::getline(file, str))
+    std::cout << task(str);
+  return 0;
 }
